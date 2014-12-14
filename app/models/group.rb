5e1a62f817bfd5880cd1480317f9e010e8936f07
szewczykmira <szewczyk.mira@gmail.com>
2014-12-14 23:43:01 +0100

    Validation

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,13 @@
 class Group < ActiveRecord::Base
+  validates :name, presence: true
+  validates :person_id, presence: true
+
+  validate :person_is_teacher
+
+  has_many :peoples
+
+  private
+  def person_is_teacher
+    Person.find(:person_id).is_teacher?
+  end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,12 @@
 class Person < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :second_name, presence: true
+  validates :birth_date, presence: true
+  validates :group_id, presence: true
+
+  belongs_to :group
+
+  def is_teacher?
+    :is_teacher
+  end
 end
