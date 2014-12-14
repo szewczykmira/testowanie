7735f20e1492a19a9207e6852b254462be106448
szewczykmira <szewczyk.mira@gmail.com>
2014-12-14 23:43:01 +0100

    Validation

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,10 @@
 class Person < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :second_name, presence: true
+  validates :birth_date, presence: true
+  validates :group_id, presence: true
+
+  def is_teacher?
+    :is_teacher
+  end
 end
