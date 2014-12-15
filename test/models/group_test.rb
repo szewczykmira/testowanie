7c65bb998bbe5e688687c648fd7d3cb0be7aa394
szewczykmira <szewczyk.mira@gmail.com>
2014-12-15 17:07:20 +0100

    Model test

--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class GroupTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'group requires name' do
+    group = Group.new(person_id: 1, profile: 'XYZ')
+    assert_not group.valid?, 'Name is required'
+  end
+
+  test 'group requires person_id' do
+    group = Group.new(person_id: 7)
+    assert_not group.valid?, 'Person_id is required'
+  end
+
+  test 'valid group' do
+    group = Group.new(name: 'Ii', person_id: 7)
+    assert group.valid?
+  end
 end
--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'person requires first_name' do
+    person = Person.new(second_name: 'Smith', birth_date: '19-09-1991', group_id:3)
+    assert_not person.valid?
+  end
+  test 'person requires second_name' do
+    person = Person.new(first_name: 'John', birth_date: '19-09-1991', group_id:7)
+    assert_not person.valid?
+  end
+  test 'person requires birth date' do
+    person = Person.new(first_name: 'John', second_name: 'Smith')
+    assert_not person.valid?
+  end
+  test 'student requires group_id' do
+    person = Person.new(first_name: 'John', second_name: 'Smith', birth_date:'19-09-1991', is_teacher:false)
+    assert_not person.valid?
+  end
+  test 'teacher do not need group_id' do
+    person = Person.new(first_name: 'John', second_name: 'Smith', birth_date: '19-09-1991', is_teacher:true)
+    assert person.valid?
+  end
+  test 'valid student' do
+    person = Person.new(first_name: 'John', second_name: 'Smith', birth_date: '19-09-1991', group_id:3, is_teacher: false)
+    assert person.valid?
+  end
 end
