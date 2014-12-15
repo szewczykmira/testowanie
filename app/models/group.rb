3bd0626406a713d6594650b87fcee6877ad955a3
szewczykmira <szewczyk.mira@gmail.com>
2014-12-15 17:06:57 +0100

    fix

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,6 @@ class Group < ActiveRecord::Base
   validates :name, presence: true
   validates :person_id, presence: true
 
-  validate :person_is_teacher
-
-  has_many :peoples
+  has_many :people
+  accepts_nested_attributes_for :people
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -2,7 +2,6 @@ class Person < ActiveRecord::Base
   validates :first_name, presence: true
   validates :second_name, presence: true
   validates :birth_date, presence: true
-  #jakas magia
   validates :group_id, presence: true, :if => :is_not_teacher?
 
   belongs_to :group
